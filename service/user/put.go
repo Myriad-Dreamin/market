1e7eeb4808f2b6e6a62cf754a396dbafa38cfd91
Myriad-Dreamin <camiyoru@gmail.com>
2020-01-09 21:11:16 +0800

    add put check phone

--- service/user/put.go
+++ service/user/put.go
@@ -3,14 +3,25 @@ package userservice
 import (
 	"github.com/Myriad-Dreamin/market/lib/controller"
 	"github.com/Myriad-Dreamin/market/model"
+	"net/http"
+	"github.com/Myriad-Dreamin/market/types"
 )
 
 type PutRequest struct {
 	Phone string `form:"phone" json:"phone"`
 }
 
-func (srv *Service) fillPutFields(_ controller.MContext, user *model.User, req *PutRequest) (fields []string) {
+func (srv *Service) fillPutFields(c controller.MContext, user *model.User, req *PutRequest) (fields []string) {
 	if len(req.Phone) != 0 {
+		if sug := CheckPhone(req.Phone); len(sug) != 0 {
+			c.AbortWithStatusJSON(http.StatusOK, types.ErrorSerializer{
+				Code:  types.CodeBadPhone,
+				Error: sug,
+			})
+			return
+		}
+
+
 		user.Phone = req.Phone
 		fields = append(fields, "phone")
 	}
